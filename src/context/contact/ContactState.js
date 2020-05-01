fcfbf96c1822dee2d1d6bec4550e079b5837f96e
Mustafa Hayati <mustafa.hayati1992@gmail.com>
2020-05-02 00:08:58 +0430

    Create ContactForm Component and Add it to Home component.

--- src/context/contact/ContactState.js
+++ src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from "react";
-import uuid from "uuid";
+import { v4 as uuidv4 } from "uuid";
 import ContactContext from "./contactContext";
 import ContactReducer from "./contactReducer";
 import {
@@ -42,6 +42,14 @@ const ContactState = props => {
   const [state, dispatch] = useReducer(ContactReducer, initialState);
 
   //   Add Contact
+  const addContact = contact => {
+    // contact.id = uuidv4();
+    contact.id = 5;
+    dispatch({
+      type: ADD_CONTACT,
+      payload: contact,
+    });
+  };
 
   // Delete Contact
 
@@ -59,6 +67,7 @@ const ContactState = props => {
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
+        addContact,
       }}
     >
       {props.children}
